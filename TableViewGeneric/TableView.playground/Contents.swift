5ed95f6d789936a7e9fe1256f25fb0b0a5e4c120
paprikaLang <3060866223@qq.com>
2018-01-20 16:01:27 +0800

    panio-fix

---
+++ TableViewGeneric/TableView.playground/Contents.swift
@@ -0,0 +1,31 @@
+//: Playground - noun: a place where people can play
+import PlaygroundSupport
+import UIKit
+struct Episode {
+    let title: String
+}
+
+final class EpisodesViewController: UITableViewController {
+    var episodes: [Episode] = []
+    let reuseIdentifier = "Cell"
+    override func viewDidLoad() {
+        tableView.register(UITableViewCell.self,forCellReuseIdentifier: reuseIdentifier)
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return episodes.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        let item = episodes[indexPath.row]
+        cell.textLabel?.text = item.title
+        return cell
+    }
+}
+
+let sampleEpisodes = [Episode(title:"First Episode"),Episode(title: "Second Episode"),Episode(title:"Third Episode")]
+
+let episodeVC = EpisodesViewController()
+episodeVC.episodes = sampleEpisodes
+episodeVC.view.frame = CGRect(x: 0, y: 0, width: 320, height: 480)
+PlaygroundPage.current.liveView = episodeVC.view
+
